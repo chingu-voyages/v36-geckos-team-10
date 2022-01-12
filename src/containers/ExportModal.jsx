7d07dbdfc31809dfe7556ef76614596766e9c688
TimKlancisar <96549344+TimKlancisar@users.noreply.github.com>
2022-01-12 19:56:49 +0100

    Finished drag&drop functionality
    Added drag&drop, removed dragula, modified index.css

---
+++ src/containers/ExportModal.jsx
@@ -0,0 +1,11 @@
+export const ExportModal= () => {
+    return (
+        <section>
+           {/*
+            Preview of the template code goes here.
+           */} 
+        </section>
+    );
+}
+
+export default ExportModal;
