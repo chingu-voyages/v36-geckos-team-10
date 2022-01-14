c2ee39cbf5e0d166784a9d1f83da604cea168de8
Massimiliano <massimiliano.albini@gmail.com>
2022-01-14 09:34:44 +0100

    delete button fixed

--- src/components/BuilderComponent.jsx
+++ src/components/BuilderComponent.jsx
@@ -1,26 +1,20 @@
 import { useEffect } from "react";
 
 const BuilderComponent = ({tags = 'No tag', id, buildingBlocks, setBuildingBlocks}) => {
-    const click = () => {
-        console.log(buildingBlocks)
-        const newBlocks = buildingBlocks;
-        newBlocks.splice(id, 1)
-        const remove = () => (setBuildingBlocks(newBlocks))
-        return remove
-    }
+const click = () => {setBuildingBlocks(buildingBlocks.filter((block, index) => index !== id))}
 
     return (
         /2$/.test(tags) ? <div className="template" draggable='true'>
-                          <div className="element">{tags}<button>test</button></div>
-                          <div className="element">{tags}<button>test</button></div>
+                          <div className="element">{tags}<button onClick={click}>delete</button></div>
+                          <div className="element">{tags}<button onClick={click}>delete</button></div>
                           </div> :
         /3$/.test(tags) ? <div className="template" draggable='true'>
-                          <div className="element">{tags}<button>test</button></div>
-                          <div className="element">{tags}<button>test</button></div>
-                          <div className="element">{tags}<button>test</button></div>
+                          <div className="element">{tags}<button onClick={click}>delete</button></div>
+                          <div className="element">{tags}<button onClick={click}>delete</button></div>
+                          <div className="element">{tags}<button onClick={click}>delete</button></div>
                           </div> :
         <div className="template" draggable='true'>
-        <div className="element">{tags}<button onClick={click} >test</button></div>
+        <div className="element">{tags}<button onClick={click} >d</button></div>
         </div>
     );
 }
